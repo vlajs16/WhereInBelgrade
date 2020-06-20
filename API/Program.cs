cc3c5dc8ab8331747bb5d8aae649e3e8b4125e27
vlajs16 <vladimir.belca@gmail.com>
2020-06-20 14:02:47 +0200

    Seed setted

--- API/Program.cs
+++ API/Program.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessLayer;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SeedData;
 
 namespace API
 {
@@ -13,7 +17,27 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<BeogradContext>();
+                    context.Database.Migrate();
+                    Seed.SeedUsers(context);
+                    Seed.SeedCategories(context);
+                    Seed.SeedEvents(context);
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured durign migration");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ SeedData/Seed.cs
@@ -0,0 +1,79 @@
+﻿using DataAccessLayer;
+using Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SeedData
+{
+    public class Seed
+    {
+
+        public static void SeedEvents(BeogradContext context)
+        {
+            if (!context.Dogadjaji.Any())
+            {
+                var eventsData = System.IO.File.ReadAllText("../SeedData/Data/EventsSeedJson.json");
+                var events = JsonConvert.DeserializeObject<List<Dogadjaj>>(eventsData);
+
+                foreach (var ev in events)
+                {
+                    foreach (var dog in ev.KategorijeDogadjaji)
+                    {
+                        dog.Kategorija = context.Kategorije.FirstOrDefault(x => x.Naziv == dog.Kategorija.Naziv);
+                    }
+                    context.Dogadjaji.Add(ev);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public static void SeedCategories(BeogradContext context)
+        {
+            if (!context.Kategorije.Any())
+            {
+                var categoryData = System.IO.File.ReadAllText("../SeedData/Data/CategorySeedJson.json");
+                var categories = JsonConvert.DeserializeObject<List<Kategorija>>(categoryData);
+
+                foreach (var cat in categories)
+                {
+                    context.Kategorije.Add(cat);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public static void SeedUsers(BeogradContext context)
+        {
+            if (!context.Korisnici.Any())
+            {
+                var usersData = System.IO.File.ReadAllText("../SeedData/Data/UserSeedJson.json");
+                var users = JsonConvert.DeserializeObject<List<Korisnik>>(usersData);
+
+                foreach (var user in users)
+                {
+                    byte[] passwordHash, passwordSalt;
+                    CreatePasswordHash("1234", out passwordHash, out passwordSalt);
+
+                    user.PasswordHash = passwordHash;
+                    user.PasswordSalt = passwordSalt;
+
+                    context.Korisnici.Add(user);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+
+    
+}
